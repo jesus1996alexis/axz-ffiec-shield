4ed340e98fba880955dde1bccbbb68bd230219cd
jesus1996alexis <1996jesus1996alexis1996@gmail.com>
2026-06-26 11:44:10 -0600

    build: initialize Maven project

---
+++ src/main/java/com/axz/ffiec/FfiecShieldApplication.java
@@ -0,0 +1,13 @@
+package com.axz.ffiec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfiecShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfiecShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axz/ffiec/FfiecShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axz.ffiec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfiecShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
